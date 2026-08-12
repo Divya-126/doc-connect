a6156a89a4def943925a1e512a02c7c4674e333e
Divya-126 <dhotedivya126@gmail.com>
2026-08-12 21:40:39 +0530

    new

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -11,7 +11,7 @@ import { AppContext } from "../context/AppContext.jsx";
 const ForgotPassword = () => {
   const { backendUrl } = useContext(AppContext);
 
-  console.log(backendurl);
+  console.log(backendUrl);
   const [state, setState] = useState("email");
   const [open, setOpen] = useState(false);
   const [openCon, setOpenCon] = useState(false);
