14f88ec800efb7153ceb42ee4adb99260253294b
Divya-126 <dhotedivya126@gmail.com>
2026-08-14 13:11:38 +0530

    api change

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   process.env.FRONTEND_URL,
-  process.env.ADMIN_URL,
+  // process.env.ADMIN_URL,
 ].filter(Boolean);
 
 app.use(
